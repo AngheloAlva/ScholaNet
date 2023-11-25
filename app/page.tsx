360b186b98173e8b459468142712937c5bf3f220
Anghelo Alva <ance.anghelo@gmail.com>
2023-11-24 21:56:03 -0300

    Add footer and additional course data

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import CourseSlider from '@/components/home/Course-slider'
+import Footer from '@/components/home/footer/Footer'
 import Navbar from '@/components/home/navbar/Navbar'
 import Image from 'next/image'
 
 import primaryCourses from '@/data/primary-courses.json'
+import secondayCourses from '@/data/secondary-courses.json'
+import specialCourses from '@/data/special-courses.json'
 
 function Home (): React.ReactElement {
   return (
@@ -19,7 +22,7 @@ function Home (): React.ReactElement {
         />
       </div>
 
-      <section className='p-5'>
+      <section className='p-5 mt-5'>
         <h2 className='text-3xl font-bold mb-2'>Our Programs for Basic Education</h2>
         <div className='flex'>
           <CourseSlider courses={primaryCourses} />
@@ -27,9 +30,20 @@ function Home (): React.ReactElement {
       </section>
 
       <section className='p-5'>
-        <h2 className='text-3xl font-bold'>Special Programs</h2>
-        {/* Add content or components related to your special programs here */}
+        <h2 className='text-3xl font-bold mb-2'>Our Programs for Secondary Education</h2>
+        <div className='flex'>
+          <CourseSlider courses={secondayCourses} />
+        </div>
       </section>
+
+      <section className='p-5'>
+        <h2 className='text-3xl font-bold'>Special Programs</h2>
+        <div className='flex'>
+          <CourseSlider courses={specialCourses} />
+        </div>
+      </section>
+
+      <Footer />
     </>
   )
 }
